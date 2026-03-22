7d0df8b3e9c826603f077d57e422bdc8f158e29d
Noctiro <49872497+noctiro@users.noreply.github.com>
2026-03-22 17:35:05 +0800

    Add potion of instant health with a level of 125 pix

--- src/main/java/org/encinet/mik/module/GrieferModule.java
+++ src/main/java/org/encinet/mik/module/GrieferModule.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GrieferModule implements Listener {
@@ -24,6 +25,7 @@ public class GrieferModule implements Listener {
         plugin.getLogger().info("GrieferModule enabled");
     }
 
+    // fix: generic.kill effect on creative/spectator player
     @EventHandler
     public void onPlayerJoin(EntityDamageByEntityEvent event) {
         // 攻击者是玩家
@@ -33,6 +35,14 @@ public class GrieferModule implements Listener {
 
         if (victim.getGameMode() == GameMode.CREATIVE || victim.getGameMode() == GameMode.SPECTATOR) {
            event.setCancelled(true);
+        }
+    }
+
+    // fix: /give @s splash_potion[minecraft:potion_contents={potion: "minecraft:water", custom_effects: [{show_icon: 1b, amplifier: 125b, id: "minecraft:instant_health"}]}]
+    @EventHandler
+    public void onEntityRegainHealth(EntityRegainHealthEvent e) {
+        if (e.getAmount() < 0) {
+            e.setCancelled(true);
         }
     }
 }
