7f9fdf5045273db83a41506ecd441ab5f711c0a2
Noctiro <49872497+noctiro@users.noreply.github.com>
2026-04-21 00:08:37 +0800

    add /tpany command

--- src/main/java/org/encinet/mik/module/SimpleFeaturesModule.java
+++ src/main/java/org/encinet/mik/module/SimpleFeaturesModule.java
@@ -2,6 +2,7 @@ package org.encinet.mik.module;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
@@ -11,6 +12,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
@@ -59,6 +61,44 @@ public class SimpleFeaturesModule {
                         }
                         return Command.SINGLE_SUCCESS;
                     }).build(), "返回主城", List.of("lobby"));
+
+            // Register /tpany command
+            commands.register(
+                    Commands.literal("tpany")
+                            .requires(source -> {
+                                var sender = source.getSender();
+                                return sender instanceof Player && sender.hasPermission("group.helper");
+                            })
+                            .then(
+                                    Commands.argument("player", StringArgumentType.word())
+                                            .executes(ctx -> {
+                                                Player self = (Player) ctx.getSource().getSender();
+                                                String targetName = StringArgumentType.getString(ctx, "player");
+
+                                                Player online = Bukkit.getPlayerExact(targetName);
+                                                if (online != null) {
+                                                    self.teleportAsync(online.getLocation());
+                                                    self.sendMessage("已传送到 " + targetName + " 当前位置");
+                                                    return 1;
+                                                }
+
+                                                OfflinePlayer target = Bukkit.getOfflinePlayerIfCached(targetName);
+                                                Location loc = target != null ? target.getLocation() : null;
+                                                if (loc == null) {
+                                                    self.sendMessage("找不到 " + targetName + " 的位置");
+                                                    return 0;
+                                                }
+
+                                                self.teleportAsync(loc);
+                                                self.sendMessage(String.format("已传送到 %s 的下线位置：%s (%.1f, %.1f, %.1f)",
+                                                        targetName, loc.getWorld().getName(),
+                                                        loc.getX(), loc.getY(), loc.getZ()));
+                                                return 1;
+                                            })
+                            )
+                            .build(),
+                    "传送到玩家位置（在线/离线均可）"
+            );
 
             // Register /trash command
             commands.register(Commands.literal("trash")
