a4b4cde921906c351bbe542fb7fdd366b14f56a3
Noctiro <49872497+noctiro@users.noreply.github.com>
2026-06-26 15:56:43 +0800

    Map domain

--- src/main/java/org/encinet/mik/module/player/MainMenuModule.java
+++ src/main/java/org/encinet/mik/module/player/MainMenuModule.java
@@ -57,7 +57,7 @@ public class MainMenuModule implements Listener {
     private static final String ACTION_OPEN_WIKI = "open:url:wiki";
     private static final String ACTION_COMMAND_PREFIX = "command:";
     private static final String URL_WEBSITE = "https://mcmik.top";
-    private static final String URL_MAP = "http://203.135.99.76:34567";
+    private static final String URL_MAP = "https://mcmik.top/map";
     private static final String URL_WIKI = "https://mcmik.top/wiki";
 
     private final JavaPlugin plugin;
